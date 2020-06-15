000b5a39cc077c4f0a1ad9b0b5b8d2902a9da07f
Shan <shan.mi.fanfan@gmail.com>
2020-06-15 11:33:07 +0200

    Redirect added
    If user is signed in, page will be redirected to home if user try to get
    access to log-in page.
    Connect(will return a HOC) and Redirect is implemented.
    (通过这个HOC方法，监听redux store，然后把下级组件需要的state(通过mapStateToProps获取)和action
    creator(通过mapDispatchToProps获取)绑定到wrappedComponent的props上)。

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,3 +1,5 @@
+import { UserActionTypes } from "./user.types";
+
 const INITIAL_STATE = {
   currentUser: null,
 };
@@ -5,7 +7,7 @@ const INITIAL_STATE = {
 // set default value for state, if there is no state for the first time
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "SET_CURRENT_USER":
+    case UserActionTypes.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload,
