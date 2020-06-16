df80e50503058ae010fd5a0ef1893e76abfae5d4
Shan <shan.mi.fanfan@gmail.com>
2020-06-16 13:47:02 +0200

    Reselect and memoization
    Updating with reselect (take the whole state and select just piece of
    it, only one layer deep usually, repeat multiple times when needed);
    replaced cart selectors with memoized selectors

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,25 @@
+import { createSelector } from "reselect";
+
+// 1. input select: doesn't use create selector
+// 2. output select: does use input selectors and creates selector to build themselves
+
+// take the whole state and select just piece of it, only one layer deep usually
+// here we just want to get state's property cart
+const selectCart = (state) => state.cart;
+
+// export the property of cart, if we take multiple properties, then we have to make sure the second parameter is corresponsive with the first parameter.
+// now it is a memoized selector
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0
+    )
+);
