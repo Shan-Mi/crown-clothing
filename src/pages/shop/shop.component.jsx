1f662207a8d0cadac8b5bf7c364bd4024922a5af
Shan <shan.mi.fanfan@gmail.com>
2020-06-28 11:39:46 +0200

    Data normalization and etc
    Moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with redux flow for directory/shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -8,7 +8,7 @@ import CollectionPage from "../collection/collection.component";
 
 // we can get match history location from app
 const ShopPage = ({ match }) => {
-  console.log(match);
+  // console.log(match);
   return (
     <div className="shop-page">
       <Route exact path={`${match.path}`} component={CollectionsOverview} />
