166cb97316393bd3a3e6528e30c45efa0475a8bb
Shan <shan.mi.fanfan@gmail.com>
2020-07-21 10:08:08 +0200

    Use useEffect to mimic componentWillUnmount
    Demonstration about how to return an unsubscribe function inside of the useEffect hook to mimic componentWillUnmount, and also we need to set an empty [] in the end. Technically it is still the listener model, and still we will get snapshot back as before.
    Later these codes will be removed.

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { connect } from "react-redux";
 
 import CollectionItem from "../../components/collection-item/collection-item.component";
 import { selectCollection } from "../../redux/shop/shop.selector";
-
+import { firestore } from "../../firebase/firebase.utils.js";
 import "./collection.styles.scss";
 
 const CollectionPage = ({ collection }) => {
+  useEffect(() => {
+    console.log("I am subscribing");
+    const unsubscribeFromCollections = firestore
+      .collection("collections")
+      .onSnapshot((snapshot) => console.log(snapshot));
+
+    return () => {
+      console.log("I am unsbuscribing");
+      unsubscribeFromCollections();
+    };
+  },[]);
+
   const { title, items } = collection;
   return (
     <div className="collection-page">
