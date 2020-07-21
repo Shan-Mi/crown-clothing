39a5ab7e0626b7f99618a4f50a638128f9e21b0c
Shan <shan.mi.fanfan@gmail.com>
2020-07-21 16:41:44 +0200

    Seperating files
    Crown clothing app has been seperated into client folded;
    Added express server to help handle backend stripe payment.

---
+++ client/build/precache-manifest.59a434f84c646fb06f3488694f8393b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50c3175fb010fef65e70059a7fb2e12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "091131e8b9e24827634f",
+    "url": "/static/css/main.1bfb4eb2.chunk.css"
+  },
+  {
+    "revision": "cd4cb1b7fc83023327b5",
+    "url": "/static/js/2.8145c391.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.8145c391.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "091131e8b9e24827634f",
+    "url": "/static/js/main.ae73ba97.chunk.js"
+  },
+  {
+    "revision": "6c30a7a6be8872f21e40",
+    "url": "/static/js/runtime-main.b718eb02.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
