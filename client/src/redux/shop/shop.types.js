03516409446b115d18a6b674acd2de5fddd4d7fb
Shan <shan.mi.fanfan@gmail.com>
2020-07-23 09:47:23 +0200

    context-api branch

--- client/src/redux/shop/shop.types.js
+++
@@ -1,8 +0,0 @@
-// Different stages of middleware
-const ShopActionTypes = {
-  FETCH_COLLECTIONS_START: "FETCH_COLLECTIONS_START",
-  FETCH_COLLECTIONS_SUCCESS: "FETCH_COLLECTIONS_SUCCESS",
-  FETCH_COLLECTIONS_FAILURE: "FETCH_COLLECTIONS_FAILURE",
-};
-
-export default ShopActionTypes;
