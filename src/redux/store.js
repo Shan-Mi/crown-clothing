4048129fcc778e9f403887b16fb5d6cb791b3857
Shan <shan.mi.fanfan@gmail.com>
2020-07-25 22:42:08 +0200

    Temperarily replaced middlewarew with redux-devToolExtension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,24 @@
-import { createStore, applyMiddleware } from 'redux';
-import { persistStore } from 'redux-persist';
-import logger from 'redux-logger';
+import { createStore, applyMiddleware, compose } from "redux";
+import { persistStore } from "redux-persist";
+import logger from "redux-logger";
 
-import rootReducer from './root-reducer';
+import rootReducer from "./root-reducer";
 
 const middlewares = [];
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(
+  rootReducer,
+  compose(
+    typeof window === "object" &&
+      typeof window.devToolsExtension !== "undefined"
+      ? window.devToolsExtension()
+      : (f) => f
+  )
+);
 
 export const persistor = persistStore(store);
 
