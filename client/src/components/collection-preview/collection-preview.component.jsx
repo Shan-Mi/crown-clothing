87094717e7d494b3e8571e0b87c5cfca8e85e185
Shan <shan.mi.fanfan@gmail.com>
2020-08-11 16:57:28 +0200

    Performance optimizations
    Added relevant optimizations to codebase with react lazy, suspense and memo.

--- client/src/components/collection-preview/collection-preview.component.jsx
+++ client/src/components/collection-preview/collection-preview.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import CollectionItem from "../collection-item/collection-item.component";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import {
   CollectionPreviewContainer,
   TitleContainer,
